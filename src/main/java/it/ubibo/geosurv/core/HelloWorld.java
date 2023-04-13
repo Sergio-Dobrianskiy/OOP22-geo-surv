a126f5d5994bb14c01909bb9f21f95fc60490ad7
Sergio Dobrianskiy <sergio.dobrianskiy@studio.unibo.it>
2023-04-13 17:24:00 +0200

    created basic project structure + build paths

---
+++ src/main/java/it/ubibo/geosurv/core/HelloWorld.java
@@ -0,0 +1,7 @@
+package it.ubibo.geosurv.core;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
---
+++ src/main/java/it/unibo/geosurv/utility/EmpltyClass.java
@@ -0,0 +1,5 @@
+package it.unibo.geosurv.utility;
+
+public class EmpltyClass {
+
+}
