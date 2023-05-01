a95db61603bd3eb384d08af03645cb1300914464
Pierangelo-Motta <pierangelo.motta@studio.unibo.it>
2023-05-01 16:32:02 +0200

    Added Monster interface and class

---
+++ src/main/java/it/unibo/geosurv/core/Monster.java
@@ -0,0 +1,11 @@
+package it.unibo.geosurv.core;
+
+public interface Monster extends Entity {
+
+    /**
+     * Allow monster to try to reach the target/player
+     * 
+     */
+    void reachTarget();
+
+}
---
+++ src/main/java/it/unibo/geosurv/core/MonsterImpl.java
@@ -0,0 +1,43 @@
+package it.unibo.geosurv.core;
+
+import it.unibo.geosurv.weapons.Weapon;
+
+public class MonsterImpl implements Monster {
+
+    @Override
+    public Entity born() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'born'");
+    }
+
+    @Override
+    public boolean isDead() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isDead'");
+    }
+
+    @Override
+    public Experience dropExperience() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'dropExperience'");
+    }
+
+    @Override
+    public void takeHit(Weapon weapon) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'takeHit'");
+    }
+
+    @Override
+    public void reachTarget() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'reachTarget'");
+    }
+
+    @Override
+    public int getHealth() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getHealth'");
+    }
+
+}
