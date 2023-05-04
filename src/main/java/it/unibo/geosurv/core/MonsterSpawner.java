e454a97d1d826afe865c334276a56daaa07d4089
Pierangelo-Motta <pierangelo.motta@studio.unibo.it>
2023-05-04 18:41:29 +0200

    MonsterSpawner: creating 10 triangle monster from position [0,0]

--- src/main/java/it/unibo/geosurv/core/MonsterSpawner.java
+++ src/main/java/it/unibo/geosurv/core/MonsterSpawner.java
@@ -10,26 +10,27 @@ import it.unibo.geosurv.utility.Func;
 
 public class MonsterSpawner extends GameObject {
 
-    private final static int SPAWN_INTERVAL = 3000; // specifies the time interval (in milliseconds) between monster
+    private final static int SPAWN_INTERVAL = 1000; // specifies the time interval (in milliseconds) between monster
                                                     // spawns 3 secs
     private final static int MAX_MONSTERS = 10; // specifies the maximum number of monsters that can be spawned at any
                                                 // given time
     private static int numMonsters = 0;
     private static long lastSpawnTime = 0L;
     private static Random random = new Random();
-    private static Handler handler;
+    private Handler handler;
     private static GameObject tempPlayer;
     static Game game;
 
-    private MonsterSpawner(float x, float y, ID id) {
+    public MonsterSpawner(float x, float y, ID id, Handler h, Game game2) {
         super(x, y, id);
-        // TODO Auto-generated constructor stub
+        this.handler = h;
     }
 
-    public static void spawnMonsters() {
+    public void spawnMonsters() {
 
         // look for the the player
-        tempPlayer = Func.findPlayer(handler);
+        tempPlayer = Func.findPlayer(this.handler);
+        // System.out.println(tempPlayer);
 
         // Check if it's time to spawn a new monster
         long currentTime = System.currentTimeMillis();
@@ -39,8 +40,7 @@ public class MonsterSpawner extends GameObject {
             // Monster monster = null;
 
             // sure NOT WORKING
-            Monster monster = new Triangle(20, 50, ID.Monster, handler, game);
-
+            Monster monster = new Triangle(10, 10, ID.Monster, this.handler, game);
             // switch (monsterType) {
             // case 0:
             // // monster = new Triangle();
@@ -57,36 +57,43 @@ public class MonsterSpawner extends GameObject {
             // break;
             // }
             if (monster != null) {
+                System.out.println("M created: " + getNumMonsters() + " " + monster.toString());
                 // Spawn the monster
                 // Calculate a random point that is a distance of 100 pixels away from the
                 // player in a random direction
-                float angle = (float) (random.nextFloat() * 2 * Math.PI);
-                float distance = 100.0f;
-                float x = Func.findPlayer(handler).getX() + distance * (float) Math.cos(angle);
-                float y = Func.findPlayer(handler).getY() + distance * (float) Math.sin(angle);
-
-                // Check if the random point is outside of the camera view
-                // if (!isPointInCameraView(x, y, Camera cam, float xx, float yy )) {
-                // Spawn the monster at the random point
-                // monster.setPosition(x, y);
-                // Add the monster to the game world
-                // ...
+                // float angle = (float) (random.nextFloat() * 2 * Math.PI);
+                // float distance = 10.0f;
+                // float x = tempPlayer.getX() + distance * (float) Math.cos(angle);
+                // float y = tempPlayer.getY() + distance * (float) Math.sin(angle);
+                // System.out.println("x: " + x + ", y: " + y);
+                //// Check if the random point is outside of the camera view
+                //// if (!isPointInCameraView(x, y, Camera cam, float xx, float yy )) {
+                //// Spawn the monster at the random point
+                //// monster.setPosition(x, y);
+                //// Add the monster to the game world
+                monster.setX(x);
+                monster.setY(y);
+                this.handler.addObject(monster);
                 numMonsters++;
                 lastSpawnTime = currentTime;
                 // }
             }
         }
+
+    }
+
+    public static int getNumMonsters() {
+        return numMonsters;
     }
 
     @Override
     public void tick() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'tick'");
+        spawnMonsters();
     }
 
     @Override
     public void render(Graphics g) {
-        throw new UnsupportedOperationException("Unimplemented method 'render'");
+        // throw new UnsupportedOperationException("Unimplemented method 'render'");
     }
 
     @Override
