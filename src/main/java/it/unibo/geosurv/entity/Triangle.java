75c1097720385c7e67f312c11907680421e64be7
Pierangelo-Motta <pierangelo.motta@studio.unibo.it>
2023-05-04 16:25:17 +0200

    Class Triangle: re-defined (constructor, tick(), render() and bounds dimension)

--- src/main/java/it/unibo/geosurv/entity/Triangle.java
+++ src/main/java/it/unibo/geosurv/entity/Triangle.java
@@ -5,33 +5,51 @@ import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.util.LinkedList;
 
+import it.unibo.geosurv.core.Game;
 import it.unibo.geosurv.core.GameObject;
 import it.unibo.geosurv.core.Handler;
 import it.unibo.geosurv.core.ID;
+import it.unibo.geosurv.utility.Func;
 
 public class Triangle extends Monster {
 
     private Handler handler;
+    private Game game;
+    private float speed = (float) 0.5;
+    private static int counter = 0;
+    private String name = "Triangle-";
+
+    private GameObject tempPlayer;
 
     /**
-     * Triangel constructor
+     * Triangle constructor
      */
-    public Triangle(float x, float y, ID id) {
+    public Triangle(float x, float y, ID id, Handler handler, Game game) {
         super(x, y, ID.Monster);
+        this.speed = speed;
+        this.handler = handler;
+        this.game = game;
+        Triangle.counter++;
+        this.name = name + Triangle.counter;
     };
 
     @Override
     public void tick() {
         x += velX;
         y += velY;
-        LinkedList<GameObject> tmpObjects = handler.getObjects();
+        tempPlayer = Func.findPlayer(handler);
 
+        int mx = (int) this.tempPlayer.getX();
+        int my = (int) this.tempPlayer.getY();
+
+        LinkedList<GameObject> tmpObjects = handler.getObjects();
         for (int i = 0; i < tmpObjects.size(); i++) {
             GameObject tempObject = tmpObjects.get(i);
             // TODO: better use ID.weapon -> only one
-            if (tempObject.getId() == ID.Satellite) {
+            if (tempObject.getId() == ID.Player) {
                 if (this.getBounds().intersects(tempObject.getBounds())) {
-                    System.out.println(this.id + " is removed");
+                    System.out.println(this.toString() + " " + this.name + " object is removed");
+                    System.out.println("Player position: [" + mx + "," + my + "]");
                     handler.removeObject(this);
                 }
             }
@@ -40,13 +58,13 @@ public class Triangle extends Monster {
 
     @Override
     public void render(Graphics g) {
-        g.setColor(Color.blue);
-        g.fillOval((int) x, (int) y, 16, 16);
+        g.setColor(Color.red);
+        g.fillRect((int) x, (int) y, 20, 20);
     }
 
     @Override
     public Rectangle getBounds() {
-        return new Rectangle((int) x, (int) y, 16, 16);
+        return new Rectangle((int) x, (int) y, 20, 20);
     }
 
     @Override
