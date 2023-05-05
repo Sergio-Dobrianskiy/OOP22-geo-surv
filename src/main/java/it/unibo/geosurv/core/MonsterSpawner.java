89b4204677a5dadddba9696031624c49454dbc9c
Pierangelo-Motta <pierangelo.motta@studio.unibo.it>
2023-05-05 14:39:22 +0200

    MonsterSpawner: modified spawnMonsters to use handler and Func.randomPoint (@Sergio-Dobrianskiy)

--- src/main/java/it/unibo/geosurv/core/MonsterSpawner.java
+++ src/main/java/it/unibo/geosurv/core/MonsterSpawner.java
@@ -4,15 +4,16 @@ import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.util.Random;
 
+import it.unibo.geosurv.utility.Pair;
 import it.unibo.geosurv.entity.Monster;
 import it.unibo.geosurv.entity.Triangle;
 import it.unibo.geosurv.utility.Func;
 
 public class MonsterSpawner extends GameObject {
 
-    private final static int SPAWN_INTERVAL = 1000; // specifies the time interval (in milliseconds) between monster
-                                                    // spawns 3 secs
-    private final static int MAX_MONSTERS = 10; // specifies the maximum number of monsters that can be spawned at any
+    private final static int SPAWN_INTERVAL = 500; // specifies the time interval (in milliseconds) between monster
+                                                   // spawns 3 secs
+    private final static int MAX_MONSTERS = 50; // specifies the maximum number of monsters that can be spawned at any
                                                 // given time
     private static int numMonsters = 0;
     private static long lastSpawnTime = 0L;
@@ -20,6 +21,7 @@ public class MonsterSpawner extends GameObject {
     private Handler handler;
     private static GameObject tempPlayer;
     static Game game;
+    float x, y;
 
     public MonsterSpawner(float x, float y, ID id, Handler h, Game game2) {
         super(x, y, id);
@@ -34,49 +36,23 @@ public class MonsterSpawner extends GameObject {
 
         // Check if it's time to spawn a new monster
         long currentTime = System.currentTimeMillis();
-        if (currentTime - lastSpawnTime >= SPAWN_INTERVAL && numMonsters < MAX_MONSTERS) {
-            // TODO: select monster based on time from begin of game
-            // int monsterType = ...
-            // Monster monster = null;
+        for (int i = 0; i < 1000; i++) { // TODO: adapt magic number 1000
+            if (currentTime - lastSpawnTime >= SPAWN_INTERVAL && numMonsters < MAX_MONSTERS) {
+                // TODO: select monster based on time from begin of game
 
-            // sure NOT WORKING
-            Monster monster = new Triangle(10, 10, ID.Monster, this.handler, game);
-            // switch (monsterType) {
-            // case 0:
-            // // monster = new Triangle();
-            // break;
-            // case 1:
-            // // monster = new Square();
-            // break;
-            // case 2:
-            // // monster = new Polygon();
-            // break;
-            // default:
-            // // Should never happen
-            // System.err.println("Unknown monster type: " + monsterType);
-            // break;
-            // }
-            if (monster != null) {
-                System.out.println("M created: " + getNumMonsters() + " " + monster.toString());
-                // Spawn the monster
-                // Calculate a random point that is a distance of 100 pixels away from the
-                // player in a random direction
-                // float angle = (float) (random.nextFloat() * 2 * Math.PI);
-                // float distance = 10.0f;
-                // float x = tempPlayer.getX() + distance * (float) Math.cos(angle);
-                // float y = tempPlayer.getY() + distance * (float) Math.sin(angle);
-                // System.out.println("x: " + x + ", y: " + y);
-                //// Check if the random point is outside of the camera view
-                //// if (!isPointInCameraView(x, y, Camera cam, float xx, float yy )) {
-                //// Spawn the monster at the random point
-                //// monster.setPosition(x, y);
-                //// Add the monster to the game world
-                monster.setX(x);
-                monster.setY(y);
-                this.handler.addObject(monster);
+                // TODO: con x = 350 => Exception in thread "Thread-0"
+                // java.lang.IllegalArgumentException:
+                // bound must be greater than origin at
+                // java.base/jdk.internal.util.random.RandomSupport.checkRange(Unknown Source)
+                Pair<Float, Float> pair = Func.randomPoint(420.0f, 500.0f);
+                GameObject tempPlayer = handler.getPlayer();
+                x = tempPlayer.getX() + pair.getX();
+                y = tempPlayer.getY() + pair.getY();
+                this.handler.addObject(new Triangle(x, y, ID.Monster, this.handler, game));
+                // Monster monster = new Triangle(10, 10, ID.Monster, this.handler, game);
+
                 numMonsters++;
                 lastSpawnTime = currentTime;
-                // }
             }
         }
 
