e5c84143d944a2f7dd60f4bfe52ef19e9483306c
Pierangelo-Motta <pierangelo.motta@studio.unibo.it>
2023-05-05 14:25:40 +0200

    Func: randomPoint() added todo

--- src/main/java/it/unibo/geosurv/utility/Func.java
+++ src/main/java/it/unibo/geosurv/utility/Func.java
@@ -8,7 +8,7 @@ import java.util.LinkedList;
 import java.util.Random;
 
 public class Func {
-	
+
 	public static MainPlayer findPlayer(Handler handler) {
 		LinkedList<GameObject> tmpObjects = handler.getObjects();
 		for (int i = 0; i < tmpObjects.size(); i++) {
@@ -18,7 +18,7 @@ public class Func {
 		}
 		return null;
 	}
-	
+
 	public static GameObject findByID(Handler handler, ID id) {
 		LinkedList<GameObject> tmpObjects = handler.getObjects();
 		for (int i = 0; i < tmpObjects.size(); i++) {
@@ -28,22 +28,23 @@ public class Func {
 		}
 		return null;
 	}
-	
+
 	/**
-     * Return random point on a circumference or ring.
-     *
-     * @param min minimal radius
-     * @param max maximum radius
-     * @return Pair coordinates x, y
-     */
+	 * Return random point on a circumference or ring.
+	 *
+	 * @param min minimal radius
+	 * @param max maximum radius
+	 * @return Pair coordinates x, y
+	 */
 	public static Pair<Float, Float> randomPoint(final float min, final float max) {
+		// TODO: Assert.assertTrue(max > min) ....
 		Random random = new Random();
 		double angle = 2 * Math.PI * Math.random();
-		double minRandom = min / max; 		// proportion: max / 1 = min / x
-	    double radius = Math.sqrt(random.nextDouble(minRandom, 1));
-	    float x = (float) (radius * Math.cos(angle));
-	    float y = (float) (radius * Math.sin(angle));
-		
+		double minRandom = min / max; // proportion: max / 1 = min / x
+		double radius = Math.sqrt(random.nextDouble(minRandom, 1));
+		float x = (float) (radius * Math.cos(angle));
+		float y = (float) (radius * Math.sin(angle));
+
 		return new Pair<>(x * max, y * max);
 	}
 }
