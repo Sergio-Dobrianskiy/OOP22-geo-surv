800f9d2e0dc06775a2ac4d47ca37c63a15adcf52
Pierangelo-Motta <pierangelo.motta@studio.unibo.it>
2023-05-08 22:04:15 +0200

    MonsterSpawner: added Rect monsters creation

--- src/main/java/it/unibo/geosurv/model/monsters/MonsterSpawner.java
+++ src/main/java/it/unibo/geosurv/model/monsters/MonsterSpawner.java
@@ -9,6 +9,7 @@ import it.unibo.geosurv.model.Game;
 import it.unibo.geosurv.model.GameObject;
 import it.unibo.geosurv.model.Handler;
 import it.unibo.geosurv.model.ID;
+import it.unibo.geosurv.model.monsters.triangle.Rect;
 import it.unibo.geosurv.model.monsters.triangle.Triangle;
 import it.unibo.geosurv.model.utility.Func;
 import it.unibo.geosurv.model.utility.Pair;
@@ -17,9 +18,10 @@ public class MonsterSpawner extends GameObject {
 
     private final static int SPAWN_INTERVAL = 500; // specifies the time interval (in milliseconds) between monster
                                                    // spawns (500 = 2 monster each sec)
-    private final static int MAX_MONSTERS = 50; // specifies the maximum number of monsters that can be spawned at any
-                                                // given time
-
+    private final static int MAX_T_MONSTERS = 50; // specifies the maximum number of monsters that can be spawned at any
+                                                  // given time
+    private final static int MAX_R_MONSTERS = 100;
+    private final static int IILEV = 30;
     private static int numMonsters = 0;
     private static long lastSpawnTime = 0L;
     private static Random random = new Random();
@@ -41,20 +43,27 @@ public class MonsterSpawner extends GameObject {
         // Check if it's time to spawn a new monster
         long currentTime = System.currentTimeMillis();
 
-        if (currentTime - lastSpawnTime >= SPAWN_INTERVAL && numMonsters < MAX_MONSTERS) {
+        if (currentTime - lastSpawnTime >= SPAWN_INTERVAL && numMonsters < MAX_T_MONSTERS) {
             // TODO: select monster based on time from begin of game
 
-            // TODO: con x = 350 => Exception in thread "Thread-0"
-            // java.lang.IllegalArgumentException:
-            // bound must be greater than origin at
-            // java.base/jdk.internal.util.random.RandomSupport.checkRange(Unknown Source)
-            Pair<Float, Float> pair = Func.randomPoint(420.0f, 500.0f);
-
-            x = tempPlayer.getX() + pair.getX();
-            y = tempPlayer.getY() + pair.getY();
-            Stream.generate(() -> new Triangle(x, y, this.handler, game, false)).limit(1)
+            // Pair<Float, Float> pair = Func.randomPoint(420.0f, 500.0f);
+            //
+            // x = tempPlayer.getX() + pair.getX();
+            // y = tempPlayer.getY() + pair.getY();
+            Stream.generate(() -> new Triangle(tempPlayer.getX() + Func.randomPoint(420.0f, 500.0f).getX(),
+                    tempPlayer.getY() + Func.randomPoint(420.0f, 500.0f).getY(), this.handler, game, false)).limit(1)
                     .forEach(i -> handler.addObject(i)); // 1->50
             // 5->250
+
+            numMonsters++;
+            lastSpawnTime = currentTime;
+        }
+
+        if ((currentTime - lastSpawnTime) >= (SPAWN_INTERVAL * IILEV) && numMonsters < MAX_R_MONSTERS) {
+
+            Stream.generate(() -> new Rect(tempPlayer.getX() + Func.randomPoint(500.0f, 600.0f).getX(),
+                    tempPlayer.getY() + Func.randomPoint(500.0f, 600.0f).getY(), this.handler, game, false)).limit(50)
+                    .forEach(i -> handler.addObject(i));
             numMonsters++;
             lastSpawnTime = currentTime;
         }
