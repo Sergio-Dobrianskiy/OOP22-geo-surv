a082caa36b68fab3fc88b32f1b519838dbaa957a
Pierangelo-Motta <pierangelo.motta@studio.unibo.it>
2023-05-08 09:57:28 +0200

    Bullet: added damage for monster

--- src/main/java/it/unibo/geosurv/model/weapons/Bullet.java
+++ src/main/java/it/unibo/geosurv/model/weapons/Bullet.java
@@ -13,7 +13,8 @@ import it.unibo.geosurv.model.weapons.ExplosionGun.Explosion;
 public abstract class Bullet extends GameObject {
 
 	protected final Handler handler;
-	
+	private int BULLET_DAMAGE = 1;
+
 	public Bullet(float x, float y, ID id, Handler handler) {
 		super(x, y, id);
 		this.handler = handler;
@@ -23,23 +24,23 @@ public abstract class Bullet extends GameObject {
 	public void tick() {
 		x += velX;
 		y += velY;
-		
+
 		LinkedList<GameObject> tmpObjects = handler.getObjects();
 		for (int i = 0; i < tmpObjects.size(); i++) {
 			GameObject tempObject = tmpObjects.get(i);
-			
+
 			if (tempObject.getId() == ID.Block) { // if bullet touches wall => removed
 				if (this.getShape().getBounds2D().intersects(tempObject.getShape().getBounds2D())) {
 					handler.removeObject(this);
 				}
 			}
-			
+
 			if (tempObject.getId() == ID.Monster) { // if bullet touches wall => removed
 				if (this.getShape().getBounds2D().intersects(tempObject.getShape().getBounds2D())) {
-					if (! (this instanceof  Explosion)) {
+					if (!(this instanceof Explosion)) {
 						handler.removeObject(this);
 					}
-					((Monster)tempObject).kill();
+					((Monster) tempObject).hit(BULLET_DAMAGE);
 				}
 			}
 		}
