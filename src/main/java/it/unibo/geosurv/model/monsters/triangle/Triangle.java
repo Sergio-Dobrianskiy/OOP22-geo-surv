9e5c1e5df2574a59a7cc5e8db62885a4e1c63487
Pierangelo-Motta <pierangelo.motta@studio.unibo.it>
2023-05-08 09:59:34 +0200

    Triangle: added MAX_HEALTH, tempPlayer evaluation moved outside reachTarget()

--- src/main/java/it/unibo/geosurv/model/monsters/triangle/Triangle.java
+++ src/main/java/it/unibo/geosurv/model/monsters/triangle/Triangle.java
@@ -14,49 +14,53 @@ import it.unibo.geosurv.model.utility.Func;
 
 public class Triangle extends Monster {
 
-    private Handler handler;
-    private Game game;
-    private float speed = 1.5f;
+    // private Handler handler;
+    // private Game game;
+    private float speed = 1.2f;
     private static int counter = 0;
+    private int MAX_HEALTH = 5;
+    private int health;
     private String name = "Triangle-";
     private GameObject tempPlayer = Game.returnHandler().getPlayer();
 
     /**
      * Triangle constructor
      */
-    public Triangle(float x, float y, Handler handler, Game game) {
+    public Triangle(float x, float y, Handler handler, Game game) { // TODO: handler & gamo not used! to delete
         super(x, y);
         // this.speed = speed;
-        this.handler = handler;
-        this.game = game;
+        // this.handler = handler;
+        // this.game = game;
         Triangle.counter++;
         this.name = name + Triangle.counter;
+        this.health = MAX_HEALTH;
     };
 
     @Override
     public void tick() {
         reachTarget();
-//        if (this.getShape().intersects(tempPlayer.getShape())) {
-//            this.die();
-//        }
-//         LinkedList<GameObject> tmpObjects = handler.getObjects();
-//         for (int i = 0; i < tmpObjects.size(); i++) {
-//	         GameObject tempObject = tmpObjects.get(i);
-//	         
-//
-//	         if (tempObject.getId() == ID.Bullet) { 
-//		         if (this.getShape().intersects(tempObject.getShape())) {
-//		        	 // TODO: add bullet.getDamage()
-////		         System.out.println(this.toString() + " " + this.name + " object is removed");
-////		         System.out.println("Position: [" + this.x + "," + this.y + "]");
-//		         }
-//	         }
-//         }
+        // if (this.getShape().intersects(tempPlayer.getShape())) {
+        // this.die();
+        // }
+        // LinkedList<GameObject> tmpObjects = handler.getObjects();
+        // for (int i = 0; i < tmpObjects.size(); i++) {
+        // GameObject tempObject = tmpObjects.get(i);
+        //
+        //
+        // if (tempObject.getId() == ID.Bullet) {
+        // if (this.getShape().intersects(tempObject.getShape())) {
+        // // TODO: add bullet.getDamage()
+        //// System.out.println(this.toString() + " " + this.name + " object is
+        // removed");
+        //// System.out.println("Position: [" + this.x + "," + this.y + "]");
+        // }
+        // }
+        // }
     }
 
     @Override
     public void render(Graphics g) {
-        g.setColor(Color.red);
+        g.setColor(Color.GRAY);
         g.fillRect((int) x, (int) y, 20, 20);
     }
 
@@ -69,7 +73,8 @@ public class Triangle extends Monster {
     public void reachTarget() {
         x += velX;
         y += velY;
-        tempPlayer = Func.findPlayer(handler);
+        // evaluated only once at creation istead of each tick()
+        // tempPlayer = Func.findPlayer(handler);
 
         int mx = (int) this.tempPlayer.getX();
         int my = (int) this.tempPlayer.getY();
