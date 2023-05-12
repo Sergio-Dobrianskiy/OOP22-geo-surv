bb86da2f3ede12eda5b248170e38189621b306e5
ThomasTesta <79266434+ThomasTesta@users.noreply.github.com>
2023-05-12 16:36:22 +0200

    Added basic menu

---
+++ src/main/java/it/unibo/geosurv/controller/menu/MainMenuView.java
@@ -0,0 +1,49 @@
+/*
+package it.unibo.geosurv.controller.menu;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainMenuView extends JFrame {
+
+    private JFrame frame;
+    private JButton startButton;
+    private JButton closeButton;
+
+    public MainMenuView() {
+        
+        frame = new JFrame("Geo-Survivors");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+
+        JPanel panel = new JPanel();
+
+        startButton = new JButton("Start");
+        closeButton = new JButton("Close");
+
+        panel.add(startButton);
+        panel.add(closeButton);
+
+        frame.add(panel);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    public JButton getStartButton() {
+        return startButton;
+    }
+
+    public JButton getCloseButton() {
+        return closeButton;
+    }
+
+    public JFrame getFrame() {
+        return frame;
+    }
+
+}
+*/
