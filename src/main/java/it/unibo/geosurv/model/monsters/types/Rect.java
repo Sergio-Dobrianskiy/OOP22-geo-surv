022683c3194ad6d6a92d192bc014bf8cc89beb81
Pierangelo-Motta <pierangelo.motta@studio.unibo.it>
2023-06-05 17:19:25 +0200

    Default health adjustment

--- src/main/java/it/unibo/geosurv/model/monsters/types/Rect.java
+++ src/main/java/it/unibo/geosurv/model/monsters/types/Rect.java
@@ -13,7 +13,7 @@ public class Rect extends Monster {
     // private Game game;
     private static final float DEFAULT_SPEED = 2f; // default speed of Rect
     private static final float MAX_SPEED = 4f; // max speed of BIG Rect
-    private static final int DEFAULT_HEALTH = 4; // default health of Rect
+    private static final int DEFAULT_HEALTH = 7; // default health of Rect
     private static final int MAX_HEALTH = 15; // default health of BIG Rect
     private static final int DEFAULT_H_DIMENSION = 15; // default height of Rect
     private static final int MAX_H_DIMENSION = 35; // max height of (big) Rect
