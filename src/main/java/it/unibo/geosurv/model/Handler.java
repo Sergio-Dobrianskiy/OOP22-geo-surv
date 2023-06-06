1ab0f91fc2e9d435ea7e214cf85286004d4a3da2
Pierangelo-Motta <pierangelo.motta@studio.unibo.it>
2023-06-06 16:53:57 +0200

    Changed some methods from returning ojects to void in handler

--- src/main/java/it/unibo/geosurv/model/Handler.java
+++ src/main/java/it/unibo/geosurv/model/Handler.java
@@ -19,97 +19,157 @@ public class Handler implements TickingObject {
     private boolean left;
     private boolean right;
 
-
     /**
      * calls tick() for every gameObject and tickingObject.
      */
+    @Override
     public void tick() {
-        Iterator<GameObject> iterator = gameObjects.iterator();
-        while (iterator.hasNext()) {
-            GameObject tempObject = iterator.next();
+        final Iterator<GameObject> goIterator = gameObjects.iterator();
+        final Iterator<TickingObject> toIterator = tickingObjects.iterator();
+        while (goIterator.hasNext()) {
+            GameObject tempObject = goIterator.next();
             tempObject.tick();
         }
 
-        Iterator<TickingObject> it = tickingObjects.iterator();
-        while (it.hasNext()) {
-            TickingObject tempObject = it.next();
+        while (toIterator.hasNext()) {
+            TickingObject tempObject = toIterator.next();
             tempObject.tick();
         }
     }
 
-
-    public GameObject addObject(final GameObject tempObject) {
+    /**
+     * Add tempObject to gameObjects.
+     * 
+     * @param tempObject
+     */
+    public void addObject(final GameObject tempObject) {
         gameObjects.add(tempObject);
-        return tempObject;
     }
 
+    /**
+     * Remove tempObject from gameObjects.
+     * 
+     * @param tempObject
+     */
     public void removeObject(final GameObject tempObject) {
         gameObjects.remove(tempObject);
     }
 
-    public TickingObject addTickingObject(final TickingObject tempObject) {
+    /**
+     * Add tickingObject.
+     * 
+     * @param tempObject
+     */
+    public void addTickingObject(final TickingObject tempObject) {
         tickingObjects.add(tempObject);
-        return tempObject;
     }
 
+    /**
+     * Remove tickingObject.
+     * 
+     * @param tempObject
+     */
     public void removeTickingObject(final TickingObject tempObject) {
         tickingObjects.remove(tempObject);
     }
 
-    public MainPlayer addPlayer(MainPlayer player) {
+    /**
+     * Add player to gameObjects.
+     * 
+     * @param player
+     */
+    public void addPlayer(final MainPlayer player) {
         this.player = player;
         this.gameObjects.add(player);
-        return player;
     }
 
+    /**
+     * @return player.
+     */
     public MainPlayer getPlayer() {
         return this.player;
     }
 
+    /**
+     * @return gameObjects size.
+     */
     public int getObjectsSize() {
         return this.gameObjects.size();
     }
 
+    /**
+     * @return gameObjects.
+     */
     public CopyOnWriteArrayList<GameObject> getGameObjects() {
         return this.gameObjects;
     }
 
+    /**
+     * @return tickingObjects.
+     */
     public CopyOnWriteArrayList<TickingObject> getTickingbjects() {
         return this.tickingObjects;
     }
 
+    /**
+     * @return if isUp
+     */
     public boolean isUp() {
         return up;
     }
 
-    public void setUp(boolean up) {
+    /**
+     * @param up
+     */
+    public void setUp(final boolean up) {
         this.up = up;
     }
 
+    /**
+     * @return if isDown.
+     */
     public boolean isDown() {
         return down;
     }
 
-    public void setDown(boolean down) {
+    /**
+     * @param down
+     */
+    public void setDown(final boolean down) {
         this.down = down;
     }
 
+    /**
+     * @return if isLeft
+     */
     public boolean isLeft() {
         return left;
     }
 
-    public void setLeft(boolean left) {
+    /**
+     * @param left
+     */
+    public void setLeft(final boolean left) {
         this.left = left;
     }
 
+    /**
+     * @return if isRight
+     */
     public boolean isRight() {
         return right;
     }
 
-    public void setRight(boolean right) {
+    /**
+     * @param right
+     */
+    public void setRight(final boolean right) {
         this.right = right;
     }
 
+    /**
+     * Clear handler.
+     */
     public void clearHandler() {
         gameObjects.clear();
         tickingObjects.clear();
