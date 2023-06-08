8da72ebb982264c173dfeb0086689d5dffe0ef00
Sergio Dobrianskiy <sergio.dobrianskiy@studio.unibo.it>
2023-06-09 01:29:19 +0200

    added template and strategy to doc

--- src/main/java/it/unibo/geosurv/model/collisions/ICollisionBehavior.java
+++ src/main/java/it/unibo/geosurv/model/collisions/ICollisionBehavior.java
@@ -4,13 +4,15 @@ import it.unibo.geosurv.model.GameObject;
 import it.unibo.geosurv.model.Handler;
 
 /**
- * Interface for behaviors in case of collision
+ * Interface for behaviors in case of collision.
  */
 public interface ICollisionBehavior {
     // TODO: STRATEGY PATTERN
-    
+
     /**
      * behavior in case of collision.
+     * @param ths "this" class
+     * @param handler handler
      */
-    void collide(final GameObject ths, final Handler handler);
+    void collide(GameObject ths, Handler handler);
 }
